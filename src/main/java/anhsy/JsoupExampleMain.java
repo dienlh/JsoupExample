2285721fca7e3fc00e10f8f55fca203093556c62
dien le huu <dienlh1993@gmail.com>
2020-11-12 14:02:51 +0700

    start project

---
+++ src/main/java/anhsy/JsoupExampleMain.java
@@ -0,0 +1,18 @@
+package anhsy;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+public class JsoupExampleMain {
+    public static final String USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36";
+
+    public static void main(String[] args) {
+        String url = "https://google.com.vn/";
+        try {
+            Document doc = Jsoup.connect(url).userAgent(USER_AGENT).timeout(5000).get();
+            System.out.println(doc.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
